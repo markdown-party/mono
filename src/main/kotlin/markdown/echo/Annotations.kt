155f5cd3fe8b1e6e72da62be62db07618d27aeca
Alexandre Piveteau <alexandre@piveteau.email>
2021-03-04 02:57:04 +0100

    Document OptIn annotations

--- src/main/kotlin/markdown/echo/Annotations.kt
+++ src/main/kotlin/markdown/echo/Annotations.kt
@@ -14,6 +14,7 @@ import kotlin.RequiresOptIn.Level.WARNING
         make sure you understand the tradeoffs before using it.
         """,
 )
+@MustBeDocumented
 annotation class EchoEventLogPreview
 
 /**
@@ -27,4 +28,5 @@ annotation class EchoEventLogPreview
         make sure you understand the tradeoffs before using it.
 """,
 )
+@MustBeDocumented
 annotation class EchoSyncPreview
