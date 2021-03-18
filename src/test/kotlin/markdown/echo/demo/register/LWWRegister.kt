ba0319bc4a8748058c1f9014fcafccb3675569d9
Alexandre Piveteau <alexandre@piveteau.email>
2021-03-18 23:30:22 +0100

    Add some GSet and LWWRegister demos

---
+++ src/test/kotlin/markdown/echo/demo/register/LWWRegister.kt
@@ -0,0 +1,106 @@
+package markdown.echo.demo.register
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.fail
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+import markdown.echo.Echo
+import markdown.echo.causal.SiteIdentifier
+import markdown.echo.events.event
+import markdown.echo.memory.MemoryEcho
+import markdown.echo.memory.memory
+import markdown.echo.projections.OneWayProjection
+import markdown.echo.projections.projection
+import markdown.echo.sync
+
+/**
+ * A simple LWW register, where concurrent writes are resolved by looking at the highest absolute
+ * timestamp.
+ */
+private sealed class LWWRegisterEvent<out T> {
+
+  /** Sets the [value] in the register. */
+  data class Set<out T>(
+      val value: T,
+  ) : LWWRegisterEvent<T>()
+}
+/** Aggregates the [LWWProjection] events. */
+private class LWWProjection<T> : OneWayProjection<T?, LWWRegisterEvent<T>> {
+
+  override fun forward(body: LWWRegisterEvent<T>, model: T?): T? =
+      when (body) {
+        // Always pick the latest event value, which has the highest event identifier.
+        is LWWRegisterEvent.Set -> body.value
+      }
+}
+
+/** A class representing a [LWWRegister]. */
+private class LWWRegister<T>(private val echo: MemoryEcho<LWWRegisterEvent<T>>) {
+
+  /** The latest available value from the [LWWRegister]. */
+  val value: Flow<T?> = echo.projection(null, LWWProjection())
+
+  suspend fun set(value: T) {
+    // By default, events are added with a highest seqno than whatever they've received until now.
+    echo.event { yield(LWWRegisterEvent.Set(value)) }
+  }
+}
+
+class LWWRegisterTest {
+
+  @Test
+  fun `two sites eventually converge on a LWW value`(): Unit = runBlocking {
+    val alice = SiteIdentifier(123)
+    val aliceEcho = Echo.memory<LWWRegisterEvent<Int>>(alice)
+    val aliceRegister = LWWRegister(aliceEcho)
+
+    val bob = SiteIdentifier(456)
+    val bobEcho = Echo.memory<LWWRegisterEvent<Int>>(bob)
+    val bobRegister = LWWRegister(bobEcho)
+
+    aliceRegister.set(123)
+    bobRegister.set(456)
+
+    assertEquals(123, aliceRegister.value.filterNotNull().first())
+    assertEquals(456, bobRegister.value.filterNotNull().first())
+
+    // Sync for a bit.
+    withTimeoutOrNull(1000) { sync(aliceEcho, bobEcho) }
+
+    // Ensure convergence over a non-null value.
+    val shared =
+        combine(
+                aliceRegister.value.filterNotNull(),
+                bobRegister.value.filterNotNull(),
+            ) { a, b -> a to b }
+            .filter { (a, b) -> a == b }
+            .map { (a, _) -> a }
+            .first()
+
+    // Let the "other" site issue an event.
+    val register =
+        when (shared) {
+          123 -> bobRegister
+          456 -> aliceRegister
+          else -> fail("Expected convergence over 123 or 456.")
+        }
+
+    // Set the shared value and sync a bit.
+    register.set(789)
+    withTimeoutOrNull(1000) { sync(aliceEcho, bobEcho) }
+
+    // Ensure convergence over a non-null value.
+    val result =
+        combine(
+                aliceRegister.value.filterNotNull(),
+                bobRegister.value.filterNotNull(),
+            ) { a, b -> a to b }
+            .filter { (a, b) -> a == b }
+            .map { (a, _) -> a }
+            .first()
+
+    assertEquals(789, result)
+  }
+}
---
+++ src/test/kotlin/markdown/echo/demo/set/GSet.kt
@@ -0,0 +1,91 @@
+package markdown.echo.demo.set
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import markdown.echo.Echo
+import markdown.echo.causal.SiteIdentifier
+import markdown.echo.events.event
+import markdown.echo.memory.memory
+import markdown.echo.projections.OneWayProjection
+import markdown.echo.projections.projection
+import markdown.echo.sync
+
+private sealed class GSetEvent<out T> {
+  data class Add<out T>(val item: T) : GSetEvent<T>()
+}
+
+private fun <T> gSetProjection() =
+    OneWayProjection<Set<T>, GSetEvent<T>> { event, model ->
+      when (event) {
+        // Add events, and duplicate insertions.
+        is GSetEvent.Add -> model + event.item
+      }
+    }
+
+class GSetTest {
+
+  @Test
+  fun `one site can create a set and create new events`() = runBlocking {
+    val alice = SiteIdentifier.random()
+    val echo = Echo.memory<GSetEvent<Int>>(site = alice)
+
+    echo.event {
+      yield(GSetEvent.Add(1))
+      yield(GSetEvent.Add(2))
+      yield(GSetEvent.Add(3))
+    }
+
+    val result = echo.projection(emptySet(), gSetProjection()).first { it.size == 3 }
+    assertEquals(setOf(1, 2, 3), result)
+  }
+
+  @Test
+  fun `two sites can create a shared set and eventually sync`() = runBlocking {
+    // Create Alice, our first site.
+    val alice = SiteIdentifier.random()
+    val aliceEcho = Echo.memory<GSetEvent<Int>>(site = alice)
+
+    // Create Bob, our second site.
+    val bob = SiteIdentifier.random()
+    val bobEcho = Echo.memory<GSetEvent<Int>>(site = bob)
+
+    // Alice adds the elements 1 and 2.
+    aliceEcho.event {
+      yield(GSetEvent.Add(1))
+      yield(GSetEvent.Add(2))
+    }
+
+    // Bob adds the elements 2, 3 and 4 concurrently.
+    bobEcho.event {
+      yield(GSetEvent.Add(2))
+      yield(GSetEvent.Add(3))
+      yield(GSetEvent.Add(4))
+    }
+
+    val aliceBeforeSync = aliceEcho.projection(emptySet(), gSetProjection()).first { it.size == 2 }
+    val bobBeforeSync = bobEcho.projection(emptySet(), gSetProjection()).first { it.size == 3 }
+
+    // Before sync, both sites have not merged their operations yet.
+    assertEquals(setOf(1, 2), aliceBeforeSync)
+    assertEquals(setOf(2, 3, 4), bobBeforeSync)
+
+    // Sync both sites (with a timeout, since by default they'll keep the connection open until
+    // either side cancels).
+    try {
+      withTimeout(timeMillis = 1000) { sync(aliceEcho, bobEcho) }
+    } catch (expect: TimeoutCancellationException) {}
+
+    // Finally, look at the resulting set of both sites.
+    val aliceAfterSync = aliceEcho.projection(emptySet(), gSetProjection()).first { it.size == 4 }
+    val bobAfterSync = aliceEcho.projection(emptySet(), gSetProjection()).first { it.size == 4 }
+
+    // All the events are properly synced.
+    val expected = setOf(1, 2, 3, 4)
+    assertEquals(expected, aliceAfterSync)
+    assertEquals(expected, bobAfterSync)
+  }
+}
