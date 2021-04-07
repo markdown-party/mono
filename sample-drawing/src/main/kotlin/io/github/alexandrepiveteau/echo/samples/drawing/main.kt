92738fad34dd436f336226b32ea4e3bcdae37e2c
Alexandre Piveteau <alexandre@piveteau.email>
2021-04-07 15:51:00 +0200

    Add more features to the drawing demo

--- sample-drawing/src/main/kotlin/io/github/alexandrepiveteau/echo/samples/drawing/main.kt
+++ sample-drawing/src/main/kotlin/io/github/alexandrepiveteau/echo/samples/drawing/main.kt
@@ -1,17 +1,21 @@
 package io.github.alexandrepiveteau.echo.samples.drawing
 
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddBox
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import io.github.alexandrepiveteau.echo.causal.SiteIdentifier
 import io.github.alexandrepiveteau.echo.mutableSite
@@ -20,9 +24,8 @@ import io.github.alexandrepiveteau.echo.samples.drawing.data.model.DrawingBoardP
 import io.github.alexandrepiveteau.echo.samples.drawing.data.model.DrawingEvent
 import io.github.alexandrepiveteau.echo.samples.drawing.data.model.persistentDrawingBoardOf
 import io.github.alexandrepiveteau.echo.samples.drawing.data.runServer
-import io.github.alexandrepiveteau.echo.samples.drawing.ui.Board
+import io.github.alexandrepiveteau.echo.samples.drawing.ui.features.board.Board
 import io.github.alexandrepiveteau.echo.samples.drawing.ui.features.board.dashed
-import io.github.alexandrepiveteau.echo.samples.drawing.ui.stateful.StatefulDashboard
 import io.github.alexandrepiveteau.echo.samples.drawing.ui.stateful.StatefulParticipants
 import kotlin.random.Random
 import kotlin.system.exitProcess
@@ -31,6 +34,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
+/** The main entry point of the demo program. */
 fun main(args: Array<String>) = runBlocking {
 
   // The single source of truth for the local site.
@@ -49,7 +53,7 @@ fun main(args: Array<String>) = runBlocking {
 
   // Run the GUI.
   val figures = site.value.map { it.figures }
-  Window(title = "Echo - Drawing (${config.me.name})") {
+  Window(title = "Echo - Drawing (${config.me.name})", size = IntSize(400, 400)) {
     MaterialTheme {
       Box(Modifier.fillMaxSize().dashed()) {
         val current by figures.collectAsState(persistentSetOf())
@@ -62,7 +66,13 @@ fun main(args: Array<String>) = runBlocking {
             modifier = Modifier.align(Alignment.TopEnd).padding(16.dp),
         )
 
-        // Display the figure board.
+        // Let the user add new figures.
+        FloatingActionButton(
+            onClick = { scope.launch { site.event { yield(DrawingEvent.AddFigure) } } },
+            modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp),
+        ) { Icon(Icons.Outlined.AddBox, null) }
+
+        // Display the figure board, and handle figure modifications.
         Board(
             figures = current,
             onFigureClick = {
@@ -70,26 +80,41 @@ fun main(args: Array<String>) = runBlocking {
                 val event =
                     DrawingEvent.Move(
                         it.id,
-                        toX = Random.nextInt(from = -300, until = 300).dp,
-                        toY = Random.nextInt(from = -300, until = 300).dp,
+                        toX = Random.nextInt(from = -144, until = 144).dp,
+                        toY = Random.nextInt(from = -144, until = 144).dp,
                     )
                 site.event { yield(event) }
               }
             },
+            onFigureChangeColor = {
+              scope.launch {
+                val event =
+                    DrawingEvent.SetColor(
+                        it.id,
+                        color = (FiguresColors - it.color).random(),
+                    )
+                site.event { yield(event) }
+              }
+            },
+            onFigureDelete = { fig ->
+              scope.launch { site.event { yield(DrawingEvent.Delete(fig.id)) } }
+            },
             modifier = Modifier.fillMaxSize(),
         )
-
-        // Display the debug inputs.
-        Row {
-          Button(
-              onClick = { scope.launch { site.event { yield(DrawingEvent.AddFigure) } } },
-          ) { Text("${current.size} figures") }
-          StatefulDashboard(
-              site = site,
-              modifier = Modifier.width(300.dp).background(Color.LightGray),
-          )
-        }
       }
     }
   }
 }
+
+/** The list of [Color] which will be picked for figures. */
+private val FiguresColors =
+    listOf(
+        Color(0xFFFFCDD2),
+        Color(0xFFF8BBD0),
+        Color(0xFFB39DDB),
+        Color(0xFFC5CAE9),
+        Color(0xFFB3E5FC),
+        Color(0xFFC8E6C9),
+        Color(0xFFE6EE9C),
+        Color(0xFFFFE082),
+    )
--- sample-drawing/src/main/kotlin/io/github/alexandrepiveteau/echo/samples/drawing/ui/features/board/Board.kt
+++ sample-drawing/src/main/kotlin/io/github/alexandrepiveteau/echo/samples/drawing/ui/features/board/Board.kt
@@ -1,13 +1,15 @@
-package io.github.alexandrepiveteau.echo.samples.drawing.ui
+package io.github.alexandrepiveteau.echo.samples.drawing.ui.features.board
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Palette
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
@@ -22,6 +24,8 @@ import io.github.alexandrepiveteau.echo.samples.drawing.data.model.Figure
 fun Board(
     figures: Set<Figure>,
     onFigureClick: (Figure) -> Unit,
+    onFigureChangeColor: (Figure) -> Unit,
+    onFigureDelete: (Figure) -> Unit,
     modifier: Modifier = Modifier,
 ) {
   Box(modifier, Alignment.Center) {
@@ -34,6 +38,8 @@ fun Board(
         Figure(
             color = color,
             onClick = { onFigureClick(figure) },
+            onColorClick = { onFigureChangeColor(figure) },
+            onDeleteClick = { onFigureDelete(figure) },
             modifier = Modifier.offset { IntOffset(x.roundToPx(), y.roundToPx()) },
         )
       }
@@ -45,11 +51,28 @@ fun Board(
 fun Figure(
     color: Color,
     onClick: () -> Unit,
+    onColorClick: () -> Unit,
+    onDeleteClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
   Surface(
       modifier = modifier.size(96.dp),
       color = color,
       elevation = 2.dp,
-  ) { Box(Modifier.fillMaxSize().clickable { onClick() }) }
+      shape = RoundedCornerShape(8.dp),
+  ) {
+    Box(Modifier.fillMaxSize().clickable { onClick() }) {
+      Icon(
+          imageVector = Icons.Outlined.Palette,
+          contentDescription = null,
+          modifier = Modifier.align(Alignment.TopEnd).clickable { onColorClick() }.padding(8.dp),
+      )
+      Icon(
+          imageVector = Icons.Outlined.Delete,
+          contentDescription = null,
+          modifier =
+              Modifier.align(Alignment.BottomEnd).clickable { onDeleteClick() }.padding(8.dp),
+      )
+    }
+  }
 }
