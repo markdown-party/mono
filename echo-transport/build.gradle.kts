0d52fd33965e9f0894fb8996bc8512d579977574
Alexandre Piveteau <alexandre@piveteau.email>
2021-04-21 12:25:16 +0200

    Add testing libraries to echo-transport module

--- echo-transport/build.gradle.kts
+++ echo-transport/build.gradle.kts
@@ -16,6 +16,14 @@ kotlin {
         api(Deps.Kotlinx.SerializationJson)
       }
     }
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test-common"))
+        implementation(kotlin("test-annotations-common"))
+      }
+    }
+    val jvmTest by getting { dependencies { implementation(kotlin("test-junit")) } }
+    val jsTest by getting { dependencies { implementation(kotlin("test-js")) } }
 
     all { languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes") }
   }
