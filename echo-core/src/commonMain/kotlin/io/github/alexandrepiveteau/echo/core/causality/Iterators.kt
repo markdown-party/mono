8230c74d27febb3fc3c7b664527353a6d3b9fbb2
Alexandre Piveteau <alexandre@piveteau.email>
2021-06-06 21:43:14 +0200

    Document EventIdentifierIterator

--- echo-core/src/commonMain/kotlin/io/github/alexandrepiveteau/echo/core/causality/Iterators.kt
+++ echo-core/src/commonMain/kotlin/io/github/alexandrepiveteau/echo/core/causality/Iterators.kt
@@ -1,7 +1,14 @@
 package io.github.alexandrepiveteau.echo.core.causality
 
-// TODO : Document this class.
+/**
+ * An [Iterator] that is specialized for [EventIdentifier]. On the JVM, the values will be
+ * represented as a `long`.
+ */
 interface EventIdentifierIterator : Iterator<EventIdentifier> {
+
+  /** Returns the next [EventIdentifier] for the [Iterator]. */
   override fun next(): EventIdentifier = nextEventIdentifier()
+
+  /** Returns the next [EventIdentifier] for the [Iterator]. */
   fun nextEventIdentifier(): EventIdentifier
 }
