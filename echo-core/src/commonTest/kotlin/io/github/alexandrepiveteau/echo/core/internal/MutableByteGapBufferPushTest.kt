8fdc0f61098885d6aad89b04bf1df528ff6fc8a3
Alexandre Piveteau <alexandre@piveteau.email>
2021-06-14 22:27:14 +0200

    Add tests for push() with ByteArray

--- echo-core/src/commonTest/kotlin/io/github/alexandrepiveteau/echo/core/internal/MutableByteGapBufferPushTest.kt
+++ echo-core/src/commonTest/kotlin/io/github/alexandrepiveteau/echo/core/internal/MutableByteGapBufferPushTest.kt
@@ -47,4 +47,58 @@ class MutableByteGapBufferPushTest {
       assertFails { push(0, offset = 1) }
     }
   }
+
+  @Test
+  fun pushMulti_atEnd() {
+    with(mutableByteGapBufferOf()) {
+      push(byteArrayOf(1, 2, 3))
+      assertContentEquals(byteArrayOf(1, 2, 3), toByteArray())
+    }
+  }
+
+  @Test
+  fun push_untilEnd() {
+    with(mutableByteGapBufferOf()) {
+      push(ByteArray(Gap.DefaultSize))
+      assertEquals(Gap.DefaultSize, gap.startIndex)
+      assertEquals(Gap.DefaultSize, gap.endIndex)
+    }
+  }
+
+  @Test
+  fun pushMulti_managesGapProperty() {
+    with(MutableByteGapBuffer(31) { 0 }) {
+      push(byteArrayOf(1, 1))
+      assertContentEquals(ByteArray(33) { if (it >= 31) 1 else 0 }, toByteArray())
+    }
+  }
+
+  @Test
+  fun pushMulti_atOffset() {
+    with(MutableByteGapBuffer(32) { 0 }) {
+      push(byteArrayOf(1), offset = 1)
+      assertContentEquals(ByteArray(33) { if (it == 1) 1 else 0 }, toByteArray())
+    }
+  }
+
+  @Test
+  fun pushMulti_emptyArray() {
+    with(mutableByteGapBufferOf()) {
+      push(byteArrayOf(), 0, 0, 0)
+      assertEquals(0, size)
+      push(byteArrayOf(1, 2, 3), 0, 2, 2)
+      assertEquals(0, size)
+    }
+  }
+
+  @Test
+  fun pushMulti_outOfBounds() {
+    with(mutableByteGapBufferOf()) {
+      assertFails { push(byteArrayOf(), offset = -1) }
+      assertFails { push(byteArrayOf(), offset = 1) }
+      assertFails { push(byteArrayOf(), 0, 1, 1) }
+      assertFails { push(byteArrayOf(1, 2, 3), 0, 1, 0) }
+      assertFails { push(byteArrayOf(1, 2, 3), 0, 2, 4) }
+    }
+  }
 }
