173fce112da6486c12043034b08b8934deccf168
Alexandre Piveteau <alexandre@piveteau.email>
2021-07-05 17:31:16 +0200

    Enable unit tests in `markdown` module

--- markdown/build.gradle.kts
+++ markdown/build.gradle.kts
@@ -4,15 +4,30 @@ plugins {
 }
 
 kotlin {
-  jvm()
+  jvm {
+    compilations.all { kotlinOptions.jvmTarget = "1.8" }
+    testRuns["test"].executionTask.configure { useJUnit() }
+    withJava()
+  }
   js(IR) { browser() }
 
   sourceSets {
     val commonMain by getting {
       dependencies {
+        implementation(kotlin("stdlib-common"))
         api(project(":echo"))
         api(project(":echo-transport"))
       }
     }
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test-common"))
+        implementation(kotlin("test-annotations-common"))
+      }
+    }
+    val jvmMain by getting
+    val jvmTest by getting { dependencies { implementation(kotlin("test-junit")) } }
+    val jsMain by getting
+    val jsTest by getting { dependencies { implementation(kotlin("test-js")) } }
   }
 }
