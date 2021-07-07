eedbb9f812cbfef472ecc168323904c34fcba8b8
Alexandre Piveteau <alexandre@piveteau.email>
2021-07-07 14:44:51 +0200

    Rename MutableTreeNoMoveTest and add concurrent moves test

--- markdown/src/commonTest/kotlin/party/markdown/tree/MutableTreeNoSkipTest.kt
+++ markdown/src/commonTest/kotlin/party/markdown/tree/MutableTreeNoSkipTest.kt
@@ -9,7 +9,7 @@ import party.markdown.tree.TreeEvent.*
 import party.markdown.tree.utils.TreeAggregate
 import party.markdown.tree.utils.tree
 
-class MutableTreeNoMoveTest {
+class MutableTreeNoSkipTest {
 
   @Test
   fun createFileUnnamed() {
@@ -73,6 +73,42 @@ class MutableTreeNoMoveTest {
         t3 to NewFolder,
         t4 to NewFile,
         t5 to Move(t4, t3),
+    ) { assertEquals(expected, this, "Permutation $it") }
+  }
+
+  @Test
+  fun deleteFolder() {
+    val alice = SiteIdentifier.Min
+    val bob = SiteIdentifier.Max
+    val t1 = EventIdentifier(Min + 0u, alice)
+    val t2 = EventIdentifier(Min + 1u, alice)
+    val t3 = EventIdentifier(Min + 2u, alice)
+    val t4 = EventIdentifier(Min + 2u, bob)
+    val expected = tree {}
+    TreeAggregate.permutations(
+        t1 to NewFolder,
+        t2 to NewFile,
+        t3 to Move(t2, t1),
+        t4 to Remove(t1),
+    ) { assertEquals(expected, this, "Permutation $it") }
+  }
+
+  @Test
+  fun concurrentMove() {
+    val alice = SiteIdentifier.Min
+    val bob = SiteIdentifier.Max
+    val t1 = EventIdentifier(Min + 0u, alice)
+    val t2 = EventIdentifier(Min + 0u, bob)
+    val t3 = EventIdentifier(Min + 1u, alice)
+    val t4 = EventIdentifier(Min + 2u, alice)
+    val t5 = EventIdentifier(Min + 2u, bob)
+    val expected = tree { folder(t1) { folder(t2) { file(t3) } } }
+    TreeAggregate.permutations(
+        t1 to NewFolder,
+        t2 to NewFolder,
+        t3 to NewFile,
+        t4 to Move(t2, t1),
+        t5 to Move(t3, t2),
     ) { assertEquals(expected, this, "Permutation $it") }
   }
 }
