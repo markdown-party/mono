ff40d723cbc75aaf7206727e96f3dee02be36cc0
Alexandre Piveteau <alexandre@piveteau.email>
2021-07-15 16:38:06 +0200

    Update GitHub actions workflow

--- infra/publish-images-heroku.sh
+++ infra/publish-images-heroku.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 docker tag markdown-party-backend registry.heroku.com/markdown-party/web
+docker tag markdown-party-frontend registry.heroku.com/markdown-party-frontend/web
 
 # Log in to GitHub package registry.
 echo $HEROKU_TOKEN | docker login registry.heroku.com --username=_ --password-stdin
 
 # Push the Docker images.
 docker push registry.heroku.com/markdown-party/web
+docker push registry.heroku.com/markdown-party-frontend/web
