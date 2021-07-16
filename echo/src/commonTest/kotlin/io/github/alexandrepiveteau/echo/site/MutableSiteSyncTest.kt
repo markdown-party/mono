77919bef2d1bf984b229e8b48cf0a93361f13451
Alexandre Piveteau <alexandre@piveteau.email>
2021-07-16 17:27:32 +0200

    Add bug reproducers from discarded PR #70

--- echo/src/commonTest/kotlin/io/github/alexandrepiveteau/echo/site/MutableSiteSyncTest.kt
+++ echo/src/commonTest/kotlin/io/github/alexandrepiveteau/echo/site/MutableSiteSyncTest.kt
@@ -7,6 +7,8 @@ import io.github.alexandrepiveteau.echo.protocol.Message.Incoming
 import io.github.alexandrepiveteau.echo.protocol.Message.Outgoing
 import io.github.alexandrepiveteau.echo.suspendTest
 import io.github.alexandrepiveteau.echo.sync
+import io.github.alexandrepiveteau.echo.sync.SyncStrategy.Companion.Continuous
+import io.github.alexandrepiveteau.echo.sync.SyncStrategy.Companion.Once
 import kotlin.random.Random
 import kotlin.test.Test
 
@@ -17,5 +19,38 @@ class MutableSiteSyncTest {
     val site = mutableSite<Unit>(Random.nextSiteIdentifier())
     val link = link<Incoming, Outgoing> {}
     sync(site.incoming(), link)
+  }
+
+  @Test
+  fun syncOnce_noProjection_terminates() = suspendTest {
+    val alice = mutableSite<Unit>(Random.nextSiteIdentifier(), strategy = Once)
+    val bob = mutableSite<Unit>(Random.nextSiteIdentifier(), strategy = Once)
+
+    alice.event { yield(Unit) }
+    sync(alice, bob)
+    alice.event { yield(Unit) }
+    bob.event { yield(Unit) }
+    sync(alice, bob)
+  }
+
+  @Test
+  // Bug reproducer
+  fun syncOnce_projection_terminates() = suspendTest {
+    val alice =
+        mutableSite<Unit>(
+            identifier = Random.nextSiteIdentifier(),
+            strategy = Continuous,
+        )
+    val bob =
+        mutableSite<Unit>(
+            identifier = Random.nextSiteIdentifier(),
+            strategy = Once,
+        )
+
+    alice.event { yield(Unit) }
+    sync(alice, bob)
+    alice.event { yield(Unit) }
+    // bob.event { yield(Unit) }
+    sync(alice, bob)
   }
 }
