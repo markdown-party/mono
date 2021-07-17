d98e214c4a9fa49d6357828098f1b3824bbd82d5
Alexandre Piveteau <alexandre@piveteau.email>
2021-07-17 16:50:41 +0200

    Use smaller serial names for ops

--- markdown/src/commonMain/kotlin/party/markdown/MarkdownPartyEvent.kt
+++ markdown/src/commonMain/kotlin/party/markdown/MarkdownPartyEvent.kt
@@ -1,18 +1,22 @@
 package party.markdown
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import party.markdown.rga.RGAEvent
 import party.markdown.tree.TreeEvent
 import party.markdown.tree.TreeNodeIdentifier
 
+@SerialName("e")
 @Serializable
 sealed class MarkdownPartyEvent {
 
+  @SerialName("t")
   @Serializable
   data class Tree(
       val event: TreeEvent,
   ) : MarkdownPartyEvent()
 
+  @SerialName("c")
   @Serializable
   data class RGA(
       val document: TreeNodeIdentifier,
