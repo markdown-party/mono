c2d796b72ed982e867cf35a42276c7bcdf8b1d1d
Alexandre Piveteau <alexandre@piveteau.email>
2021-07-20 00:39:08 +0200

    Fix #109 by making files and folders names editable

--- markdown-frontend/src/main/kotlin/party/markdown/ui/navigator/File.kt
+++ markdown-frontend/src/main/kotlin/party/markdown/ui/navigator/File.kt
@@ -2,15 +2,9 @@ package party.markdown.ui.navigator
 
 import kotlinx.html.Draggable
 import kotlinx.html.draggable
-import kotlinx.html.js.onClickFunction
-import kotlinx.html.js.onDragOverFunction
-import kotlinx.html.js.onDragStartFunction
-import kotlinx.html.js.onDropFunction
+import kotlinx.html.js.*
 import react.*
-import react.dom.button
-import react.dom.div
-import react.dom.img
-import react.dom.p
+import react.dom.*
 
 /**
  * Creates a new [ReactElement] that displays a file.
@@ -45,7 +39,7 @@ external interface FileProps : RProps {
   var onFileClick: () -> Unit
   var onMenuClick: () -> Unit
   var onMenuMoveToParent: () -> Unit
-  var onMenuRenameClick: () -> Unit
+  var onRenamed: (String) -> Unit
   var onMenuDeleteClick: () -> Unit
 
   // Only displayed when the displayFileType is FolderClosed or FolderOpen
@@ -84,6 +78,7 @@ private fun RBuilder.dropdownButton(
 
 private val file =
     functionalComponent<FileProps> { props ->
+      val (editedName, setEditedName) = useState<String?>(null)
       div(
           """
           flex flex-row justify-start items-center space-x-2
@@ -117,72 +112,106 @@ private val file =
           div(classes = "w-4") {}
         }
         img(src = props.iconUrl()) {}
-        p(classes = "flex-grow select-none") { +props.displayName }
-        div("""
+        if (editedName != null) {
+          input(
+              classes =
+                  """transition-all
+                 bg-gray-700 border-gray-600 hover:bg-gray-600 text-white
+                 px-2 py-1 rounded
+                 text-lg font-medium
+                 appearance-none focus:outline-none focus:border-gray-400 border-2
+                 """,
+          ) {
+            attrs {
+              this.value = editedName
+              this.onChangeFunction = { setEditedName(it.target.asDynamic().value as? String) }
+            }
+          }
+          button {
+            img(
+                src = "/icons/project-name-save.svg",
+                classes = "rounded-lg border-2 border-gray-600 hover:bg-gray-600 p-1") {
+              attrs {
+                onClickFunction =
+                    {
+                      props.onRenamed(editedName)
+                      setEditedName(null)
+                    }
+              }
+            }
+          }
+        } else {
+          p(classes = "flex-grow select-none") { +props.displayName }
+
+          div("""
             inline-block
             relative
             """) {
-          img(
-              classes =
-                  """
+            img(
+                classes =
+                    """
                 transition-all
                 opacity-0 group-hover:opacity-50
                 hover:bg-gray-800
                 rounded p-2
                 """,
-              src = "/icons/navigator-more.svg",
-          ) {
-            attrs {
-              onClickFunction =
-                  {
-                    props.onMenuClick()
-                    it.stopPropagation()
-                  }
+                src = "/icons/navigator-more.svg",
+            ) {
+              attrs {
+                onClickFunction =
+                    {
+                      props.onMenuClick()
+                      it.stopPropagation()
+                    }
+              }
             }
-          }
-          if (props.menuOpen) {
-            div(
-                """
+            if (props.menuOpen) {
+              div(
+                  """
                 origin-top-right absolute right-0 mt-2 w-64 rounded-md shadow-lg bg-white
                 ring-1 ring-black ring-opacity-5 focus:outline-none
                 z-10 text-black text-semibold
                 flex flex-col items-stretch text-lg p-2 space-y-1 text-left
                 """,
-            ) {
-              if (props.displayFileType.isFolder) {
+              ) {
+                if (props.displayFileType.isFolder) {
+                  dropdownButton(
+                      text = "New Markdown file",
+                      icon = "/icons/navigator-dropdown-new-text.svg",
+                      onClick = props.onMenuCreateMarkdownClick,
+                      classes = "hover:bg-gray-200",
+                  )
+                  dropdownButton(
+                      text = "New Folder",
+                      icon = "/icons/navigator-dropdown-new-folder.svg",
+                      onClick = props.onMenuCreateFolderClick,
+                      classes = "hover:bg-gray-200",
+                  )
+                }
                 dropdownButton(
-                    text = "New Markdown file",
-                    icon = "/icons/navigator-dropdown-new-text.svg",
-                    onClick = props.onMenuCreateMarkdownClick,
+                    text = "Rename",
+                    icon = "/icons/navigator-dropdown-rename.svg",
+                    onClick = {
+                      props.onMenuClick()
+                      setEditedName(props.displayName)
+                    },
                     classes = "hover:bg-gray-200",
                 )
+                if (props.displayIndentLevel > 0) {
+                  dropdownButton(
+                      text = "Move to parent",
+                      icon = "/icons/navigator-dropdown-move-to-parent.svg",
+                      onClick = props.onMenuMoveToParent,
+                      classes = "hover:bg-gray-200",
+                  )
+                }
                 dropdownButton(
-                    text = "New Folder",
-                    icon = "/icons/navigator-dropdown-new-folder.svg",
-                    onClick = props.onMenuCreateFolderClick,
-                    classes = "hover:bg-gray-200",
+                    text = "Delete",
+                    icon = "/icons/navigator-dropdown-delete.svg",
+                    onClick = props.onMenuDeleteClick,
+                    classes = "hover:bg-red-200 text-red-600",
                 )
               }
-              dropdownButton(
-                  text = "Rename",
-                  icon = "/icons/navigator-dropdown-rename.svg",
-                  onClick = props.onMenuRenameClick,
-                  classes = "hover:bg-gray-200",
-              )
-              if (props.displayIndentLevel > 0) {
-                dropdownButton(
-                    text = "Move to parent",
-                    icon = "/icons/navigator-dropdown-move-to-parent.svg",
-                    onClick = props.onMenuMoveToParent,
-                    classes = "hover:bg-gray-200",
-                )
-              }
-              dropdownButton(
-                  text = "Delete",
-                  icon = "/icons/navigator-dropdown-delete.svg",
-                  onClick = props.onMenuDeleteClick,
-                  classes = "hover:bg-red-200 text-red-600",
-              )
             }
           }
         }
