5b4ab4035d0bb1e5c69fb2df40ae95b64d25a4b7
Alexandre Piveteau <alexandre@piveteau.email>
2021-08-15 18:44:46 +0200

    Inline `UInt` packing

--- echo-core/src/commonMain/kotlin/io/github/alexandrepiveteau/echo/core/Packing.kt
+++ echo-core/src/commonMain/kotlin/io/github/alexandrepiveteau/echo/core/Packing.kt
@@ -5,19 +5,16 @@ package io.github.alexandrepiveteau.echo.core
 // INT <-> LONG conversions
 
 /** Packs two Int values into one Long value for use in inline classes. */
-@PublishedApi
-internal fun packUInts(val1: UInt, val2: UInt): ULong {
+internal inline fun packUInts(val1: UInt, val2: UInt): ULong {
   return val1.toULong().shl(32) or (val2.toULong() and 0xFFFFFFFFU)
 }
 
 /** Unpacks the first Int value in [packUInts] from its returned Long. */
-@PublishedApi
-internal fun unpackUInt1(value: ULong): UInt {
+internal inline fun unpackUInt1(value: ULong): UInt {
   return value.shr(32).toUInt()
 }
 
 /** Unpacks the second Int value in [packUInts] from its returned Long. */
-@PublishedApi
-internal fun unpackUInt2(value: ULong): UInt {
+internal inline fun unpackUInt2(value: ULong): UInt {
   return value.and(0xFFFFFFFFU).toUInt()
 }
