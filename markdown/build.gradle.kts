4e584127655f9bf00ba3667e1b5cdd25b97c1edf
Alexandre Piveteau <alexandre@piveteau.email>
2022-06-07 13:39:12 +0200

    Move a JVM-specific test to a common source set

--- markdown/build.gradle.kts
+++ markdown/build.gradle.kts
@@ -27,14 +27,10 @@ kotlin {
       dependencies {
         implementation(kotlin("test-common"))
         implementation(kotlin("test-annotations-common"))
-      }
-    }
-    val jvmTest by getting {
-      dependencies {
-        implementation(kotlin("test-junit"))
         implementation(libs.coroutines.test)
       }
     }
+    val jvmTest by getting { dependencies { implementation(kotlin("test-junit")) } }
     val jsMain by getting
     val jsTest by getting { dependencies { implementation(kotlin("test-js")) } }
 
--- markdown/src/commonTest/kotlin/party/markdown/MarkdownPartyEventTest.kt
+++ markdown/src/commonTest/kotlin/party/markdown/MarkdownPartyEventTest.kt
@@ -9,18 +9,15 @@ import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlinx.coroutines.test.runTest
-import party.markdown.MarkdownPartyEvent.Cursor
-import party.markdown.MarkdownPartyEvent.RGA
-import party.markdown.cursors.CursorEvent.MoveAfter
-import party.markdown.rga.RGAEvent.Insert
+import party.markdown.cursors.CursorEvent
+import party.markdown.rga.RGAEvent
 import party.markdown.rga.RGANodeRoot
 import party.markdown.tree.TreeEvent
 
-/** All the test cases that can not run on Kotlin/JS and only target Kotlin/JVM for now. */
-class Bugs {
+class MarkdownPartyEventTest {
 
   @Test
-  fun simple() = runTest {
+  fun integration() = runTest {
     val aliceHistory = MarkdownPartyHistory()
     val alice: MutableSite<MarkdownPartyEvent, MarkdownParty> =
         mutableSite(
@@ -37,15 +34,15 @@ class Bugs {
         )
 
     val file = alice.event { yield(MarkdownPartyEvent.Tree(TreeEvent.NewFile)) }
-    val a = alice.event { yield(RGA(file, Insert(RGANodeRoot, 'a'))) }
-    val b = alice.event { yield(RGA(file, Insert(a, 'b'))) }
+    val a = alice.event { yield(MarkdownPartyEvent.RGA(file, RGAEvent.Insert(RGANodeRoot, 'a'))) }
+    val b = alice.event { yield(MarkdownPartyEvent.RGA(file, RGAEvent.Insert(a, 'b'))) }
     alice.event {
-      yield(RGA(file, Insert(b, 'c')))
-      yield(Cursor(MoveAfter(file, b)))
-      yield(Cursor(MoveAfter(file, a)))
-      yield(Cursor(MoveAfter(file, b)))
-      yield(Cursor(MoveAfter(file, a)))
-      yield(Cursor(MoveAfter(file, b)))
+      yield(MarkdownPartyEvent.RGA(file, RGAEvent.Insert(b, 'c')))
+      yield(MarkdownPartyEvent.Cursor(CursorEvent.MoveAfter(file, b)))
+      yield(MarkdownPartyEvent.Cursor(CursorEvent.MoveAfter(file, a)))
+      yield(MarkdownPartyEvent.Cursor(CursorEvent.MoveAfter(file, b)))
+      yield(MarkdownPartyEvent.Cursor(CursorEvent.MoveAfter(file, a)))
+      yield(MarkdownPartyEvent.Cursor(CursorEvent.MoveAfter(file, b)))
     }
     sync(alice, bob)
     assertEquals(5, aliceHistory.size)
