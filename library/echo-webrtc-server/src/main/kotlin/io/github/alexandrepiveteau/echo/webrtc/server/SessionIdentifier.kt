05be60fdfd7cf50316b27b489603c17960a6e06b
Alexandre Piveteau <alexandre@piveteau.email>
2022-07-12 10:38:34 +0200

    Add some documentation

--- library/echo-webrtc-server/src/main/kotlin/io/github/alexandrepiveteau/echo/webrtc/server/SessionIdentifier.kt
+++ library/echo-webrtc-server/src/main/kotlin/io/github/alexandrepiveteau/echo/webrtc/server/SessionIdentifier.kt
@@ -1,4 +1,7 @@
 package io.github.alexandrepiveteau.echo.webrtc.server
 
-// TODO : Ensure the String format matches UUID v4 standard.
+/**
+ * A unique identifier for a session. Clients which join the same session will automatically connect
+ * in a peer-to-peer fashion.
+ */
 @JvmInline value class SessionIdentifier(private val id: String)
