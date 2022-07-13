15ee4405833bea07405196456538de9950669b6e
Alexandre Piveteau <alexandre@piveteau.email>
2022-07-14 00:47:50 +0200

    Add persitence to the Markdown editor

---
+++ markdown/markdown-frontend-compose/src/jsMain/kotlin/party/markdown/Persistence.kt
@@ -0,0 +1,47 @@
+package party.markdown
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import io.github.alexandrepiveteau.echo.Exchange
+import io.github.alexandrepiveteau.echo.indexeddb.load
+import io.github.alexandrepiveteau.echo.indexeddb.save
+import io.github.alexandrepiveteau.echo.protocol.Message.Incoming
+import io.github.alexandrepiveteau.echo.protocol.Message.Outgoing
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.delay
+
+/**
+ * An effect which loads the events stored in the browser in the provided [Exchange].
+ *
+ * @param session the identifier of the current session.
+ * @param exchange the [Exchange] in which the events should be loaded.
+ */
+@Composable
+fun LoadEventsEffect(session: String, exchange: Exchange<Incoming, Outgoing>) {
+  LaunchedEffect(session, exchange) {
+    try {
+      load(session, exchange)
+    } catch (_: Throwable) {
+      /*Ignored*/
+    }
+  }
+}
+
+/** The delay between each save. */
+private val SaveDelay = 10.seconds
+
+/**
+ * An effect which stores the events from the provided [Exchange] to the browser.
+ *
+ * @param session the identifier of the current session.
+ * @param exchange the [Exchange] from which the events are taken.
+ */
+@Composable
+fun SaveEventsEffect(session: String, exchange: Exchange<Incoming, Outgoing>) {
+  LaunchedEffect(session, exchange) {
+    while (true) {
+      delay(SaveDelay)
+      save(session, exchange)
+    }
+  }
+}
