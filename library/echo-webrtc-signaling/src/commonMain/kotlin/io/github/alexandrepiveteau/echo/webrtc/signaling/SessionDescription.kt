4572e07cd2bd2ea73081b0966882be1a15d41429
Alexandre Piveteau <alexandre@piveteau.email>
2022-07-13 17:12:16 +0200

    Improve `IceCandidate` and `SessionDescription` `toString` representations

--- library/echo-webrtc-signaling/src/commonMain/kotlin/io/github/alexandrepiveteau/echo/webrtc/signaling/SessionDescription.kt
+++ library/echo-webrtc-signaling/src/commonMain/kotlin/io/github/alexandrepiveteau/echo/webrtc/signaling/SessionDescription.kt
@@ -4,4 +4,8 @@ import kotlin.jvm.JvmInline
 import kotlinx.serialization.Serializable
 
 /** A session description, encoded in [json]. */
-@Serializable @JvmInline value class SessionDescription(val json: String)
+@Serializable
+@JvmInline
+value class SessionDescription(val json: String) {
+  override fun toString() = json
+}
