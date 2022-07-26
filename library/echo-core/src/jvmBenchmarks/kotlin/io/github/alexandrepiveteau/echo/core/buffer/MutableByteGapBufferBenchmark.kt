d0272f45c0b7abcc9b37ba45a026dcce1a6ac452
Alexandre Piveteau <alexandre@piveteau.email>
2022-07-26 17:04:26 +0200

    Use `Blackhole` to read the buffers and lists

--- library/echo-core/src/jvmBenchmarks/kotlin/io/github/alexandrepiveteau/echo/core/buffer/MutableByteGapBufferBenchmark.kt
+++ library/echo-core/src/jvmBenchmarks/kotlin/io/github/alexandrepiveteau/echo/core/buffer/MutableByteGapBufferBenchmark.kt
@@ -10,28 +10,32 @@ class MutableByteGapBufferBenchmark {
   private val buffer = mutableByteGapBufferOf()
 
   @Benchmark
-  fun consecutiveInsertsAtEndOfBuffer() {
+  fun consecutiveInsertsAtEndOfBuffer(hole: Blackhole) {
     buffer.clear()
     repeat(iterations) { buffer.push(it.toByte()) }
+    repeat(iterations) { hole.consume(buffer[it]) }
   }
 
   @Benchmark
-  fun consecutiveInsertsAtStartOfBuffer() {
+  fun consecutiveInsertsAtStartOfBuffer(hole: Blackhole) {
     buffer.clear()
     repeat(iterations) { buffer.push(0, offset = 0) }
+    repeat(iterations) { hole.consume(buffer[it]) }
   }
 
   private val list = mutableListOf<Byte>()
 
   @Benchmark
-  fun consecutiveInsertsAtEndOfList() {
+  fun consecutiveInsertsAtEndOfList(hole: Blackhole) {
     list.clear()
     repeat(iterations) { list.add(it.toByte()) }
+    repeat(iterations) { hole.consume(list[it]) }
   }
 
   @Benchmark
-  fun consecutiveInsertsAtStartOfList() {
+  fun consecutiveInsertsAtStartOfList(hole: Blackhole) {
     list.clear()
     repeat(iterations) { list.add(index = 0, element = 0) }
+    repeat(iterations) { hole.consume(list[it]) }
   }
 }
