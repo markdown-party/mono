c12888e5c327174bcff451b122376741bd50a03c
Alexandre Piveteau <alexandre@piveteau.email>
2022-07-29 23:48:08 +0200

    Add the `mutableTreeEventLogOf()` builder

--- library/echo-core/src/commonMain/kotlin/io/github/alexandrepiveteau/echo/core/log/Builders.kt
+++ library/echo-core/src/commonMain/kotlin/io/github/alexandrepiveteau/echo/core/log/Builders.kt
@@ -3,17 +3,21 @@ package io.github.alexandrepiveteau.echo.core.log
 import io.github.alexandrepiveteau.echo.core.causality.EventIdentifier
 import io.github.alexandrepiveteau.echo.core.log.buffer.AbstractGapBufferMutableEventLog
 import io.github.alexandrepiveteau.echo.core.log.buffer.AbstractGapBufferMutableHistory
+import io.github.alexandrepiveteau.echo.core.log.tree.AbstractAVLTreeMutableEventLog
 import kotlinx.datetime.Clock
 
-/** The actual implementation of [AbstractGapBufferMutableHistory] used in the builders. */
+/** An actual implementation of [AbstractGapBufferMutableHistory] used in the builders. */
 private class ActualMutableHistory<T>(
     initial: T,
     projection: MutableProjection<T>,
     clock: Clock,
 ) : AbstractGapBufferMutableHistory<T>(initial, projection, clock)
 
-/** The actual implementation of [AbstractGapBufferMutableEventLog] used in the builders. */
-private class ActualMutableEventLog(clock: Clock) : AbstractGapBufferMutableEventLog(clock)
+/** An actual implementation of [AbstractGapBufferMutableEventLog] used in the builders. */
+private class LinearMutableEventLog(clock: Clock) : AbstractGapBufferMutableEventLog(clock)
+
+/** An actual implementation of [AbstractAVLTreeMutableEventLog] used in the builders. */
+private class TreeMutableEventLog(clock: Clock) : AbstractAVLTreeMutableEventLog(clock)
 
 /**
  * Creates a new [MutableHistory], with an aggregate with an [initial] value and a [projection] for
@@ -61,7 +65,16 @@ public fun <T> mutableHistoryOf(
  */
 public fun mutableEventLogOf(
     clock: Clock = Clock.System,
-): MutableEventLog = ActualMutableEventLog(clock)
+): MutableEventLog = LinearMutableEventLog(clock)
+
+/**
+ * Creates a new [MutableEventLog], with no aggregate.
+ *
+ * @param clock the [Clock] used to generate new events.
+ */
+public fun mutableTreeEventLogOf(
+    clock: Clock = Clock.System,
+): MutableEventLog = TreeMutableEventLog(clock)
 
 /**
  * Creates a new [MutableEventLog], with no aggregate.
@@ -73,7 +86,23 @@ public fun mutableEventLogOf(
     vararg events: Pair<EventIdentifier, ByteArray>,
     clock: Clock = Clock.System,
 ): MutableEventLog =
-    ActualMutableEventLog(clock).apply {
+    LinearMutableEventLog(clock).apply {
+      for ((id, body) in events) {
+        insert(id.seqno, id.site, body)
+      }
+    }
+
+/**
+ * Creates a new [MutableEventLog], with no aggregate.
+ *
+ * @param events some events to pre-populate the history.
+ * @param clock the [Clock] used to generate new events.
+ */
+public fun mutableTreeEventLogOf(
+    vararg events: Pair<EventIdentifier, ByteArray>,
+    clock: Clock = Clock.System,
+): MutableEventLog =
+    TreeMutableEventLog(clock).apply {
       for ((id, body) in events) {
         insert(id.seqno, id.site, body)
       }
@@ -88,4 +117,15 @@ public fun mutableEventLogOf(
 public fun mutableEventLogOf(
     vararg events: Event,
     clock: Clock = Clock.System,
-): MutableEventLog = ActualMutableEventLog(clock).apply { for (event in events) insert(event) }
+): MutableEventLog = LinearMutableEventLog(clock).apply { for (event in events) insert(event) }
+
+/**
+ * Creates a new [MutableEventLog], with no aggregate.
+ *
+ * @param events some events to pre-populate the history.
+ * @param clock the [Clock] used to generate new events.
+ */
+public fun mutableTreeEventLogOf(
+    vararg events: Event,
+    clock: Clock = Clock.System,
+): MutableEventLog = TreeMutableEventLog(clock).apply { for (event in events) insert(event) }
