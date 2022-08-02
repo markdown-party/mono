de0d44f1dbd64a21f836664de44ff5e9dc0b515d
Alexandre Piveteau <alexandre@piveteau.email>
2022-08-02 11:44:06 +0200

    Configure benchmarks globally

---
+++ buildSrc/src/main/java/Benchmarks.kt
@@ -0,0 +1,8 @@
+import java.util.concurrent.TimeUnit
+
+object Benchmarks {
+  const val IterationsCount = 1
+  const val IterationsTime = 10L
+  val IterationsTimeUnit = TimeUnit.SECONDS.toString()
+  const val WarmupsCount = 1
+}
